305242c644d0ecb6e571b9df34e9c2703b980769
Abdo Mo <abdullah.m.jumaa@gmail.com>
2024-09-18 17:18:17 +0300

    Project Upload update requirements.txt

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ setup(
     install_requires=[
         "tensorflow==2.17.0",
         "opencv-python==4.10.0.84",
-        "matplotlib==3.9.2"
+        "matplotlib==3.9.2",
+        "uvicorn==0.30.6",
+        "python-multipart==0.0.9",
+        "fastapi==0.115.0"
+        
     ],
 
 )
